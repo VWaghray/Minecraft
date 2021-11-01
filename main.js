4021806a2807471be430609db907ace34f484af6
VWaghray <87341415+VWaghray@users.noreply.github.com>
2021-11-01 17:45:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ player_x=10;
 player_y=10;
 block_image_width=30;
 block_image_height=30;
-var player_object"";
+var player_object="";
 var block_image_object="";
 function player_update() {
   fabrc.Image.fromURL ("player.png",function(Img){
